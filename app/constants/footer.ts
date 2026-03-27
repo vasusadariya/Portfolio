21aaa7e20c2ae962a38154c60ae4513ab03a6afc
vasusadariya <sadariyavasu5@gmail.com>
2026-03-27 19:57:22 +0530

    initial commit

---
+++ app/constants/footer.ts
@@ -0,0 +1,34 @@
+import { FooterLink } from "../types";
+
+export const FOOTER_LINKS: FooterLink[] = [
+  {
+    name: 'LinkedIn',
+    hoverText: 'Connect with me',
+    icon: 'icons/linkedin.svg',
+    url: 'https://www.linkedin.com/in/vasu-sadariya-701630232/',
+  },
+  {
+    name: 'GitHub',
+    hoverText: 'Open Sourcing',
+    icon: 'icons/github.svg',
+    url: 'https://github.com/vasusadariya',
+  },
+  {
+    name: 'G-Mail',
+    hoverText: 'Send me a mail',
+    icon: 'icons/gmail.svg',
+    url: 'mailto:vasu10.nit.dev@gmail.com',
+  },
+  {
+    name: 'Instagram',
+    hoverText: 'stalk me at @vasu._.3007',
+    icon: 'icons/instagram.svg',
+    url: 'https://www.instagram.com/vasu._.3007',
+  },
+  {
+    name: 'Resume',
+    hoverText: 'Download',
+    icon: 'icons/file.svg',
+    url: './Vasu_Resume.pdf',
+  }
+];
